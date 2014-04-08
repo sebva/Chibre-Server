69f48f407d36ab366340ee7574d8d64d79fe23d2
Sébastien Vaucher <seb.vaucher@gmail.com>
2014-04-09 00:20:38 +0200

    Socket connection (crashes on reception)

--- App.xaml.cs
+++ App.xaml.cs
@@ -76,7 +76,7 @@ namespace Chibre_Server
                 // Quand la pile de navigation n'est pas restaurée, accédez à la première page,
                 // puis configurez la nouvelle page en transmettant les informations requises en tant que
                 // paramètre
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                rootFrame.Navigate(typeof(ConnectionPage), e.Arguments);
             }
             // Vérifiez que la fenêtre actuelle est active
             Window.Current.Activate();
---
+++ ConnectionManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace Chibre_Server
+{
+    class ConnectionManager
+    {
+        private static ConnectionManager instance = null;
+        private bool _acceptConnections;
+        private StreamSocketListener _socketListener;
+        private bool _receiveData = false;
+        private StreamSocket socket;
+
+        private ConnectionManager()
+        {
+            _acceptConnections = false;
+        }
+
+        public static ConnectionManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ConnectionManager();
+
+                return instance;
+            }
+        }
+
+        public async void SetAcceptConnections(bool value)
+        {
+            if(_acceptConnections ^ value) // Value changed
+            {
+                if(value == true)
+                {
+                    _socketListener = new StreamSocketListener();
+                    _socketListener.ConnectionReceived += OnConnectionReceived;
+                    await _socketListener.BindEndpointAsync(null, "24273");
+                }
+                else
+                {
+                    _socketListener.Dispose();
+                }
+
+            }
+
+            _acceptConnections = value;
+        }
+
+        public async void ReceiveLoop()
+        {
+            if (_receiveData == true)
+                return;
+
+            _receiveData = true;
+            while(_receiveData)
+            {
+                DataReader reader = new DataReader(socket.InputStream);
+                // Set inputstream options so that we don't have to know the data size
+                reader.InputStreamOptions = InputStreamOptions.Partial;
+                var data = await reader.LoadAsync(reader.UnconsumedBufferLength);
+                Debug.WriteLine(data.GetType());
+                
+            }
+        }
+
+        void OnConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            Debug.WriteLine("Someone connected");
+
+            socket = args.Socket;
+            ReceiveLoop();
+        }
+    }
+}
